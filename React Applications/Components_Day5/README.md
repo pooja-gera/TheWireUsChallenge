bdf7c35f3dc14c0bd44e036c6084c8c240c351af
sanchibansal340 <sanchibansal340@gmail.com>
2020-08-06 12:13:02 +0530

    Add Components_Day5

---
+++ React Applications/Components_Day5/README.md
@@ -0,0 +1,8 @@
+# Day 5 #
+
+## Components in React ##
+
+Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and returns HTML via a render function.
+Example: While making a website instead of adding HTML code of header/navbar again and again, you can use React to make the website. Using React, just make a Navbar/ Header component and render that component on diferent pages(like Home, About Us page etc.) of your website.
+
+Components come in two types, Class components and Function components.
---
+++ React Applications/Components_Day5/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    //render() menthod returns HTML
+    return (
+      //return has same functionality like in JS, C++, Python i.e. to return the value from a function
+      <div className="App">
+        <h1>Hello, World!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+//React DOM render() method to render the App class we created into the root div in our HTML.
+//Basically, the HTML code from App class is added into div having id=root.
