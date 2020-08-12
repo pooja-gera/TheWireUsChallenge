23bd60c209e288b1db64a6f55d584bab0bbe3681
Agam Bansal <agam35137@gmail.com>
2020-08-12 17:50:50 +0530

    Challenge 20

---
+++ JavaScript DSA/JSDSA_DAY20/README.md
@@ -0,0 +1,17 @@
+## Leaders In An Array
+
+An element is a leader in an array if it is greater than all the elements to its right side. Also the rightmost element is always a leader in an array.
+
+### Challenge: 
+
+We need to write an efficient program to solve the issue in O(n) time and O(1) space complexity.
+
+Input: arr[] = {5, 6, 1, 2, 3, 4, 1, 3}
+	
+Output: 6, 4, 3
+
+
+Input: arr[] = {16, 17, 4, 3, 5, 2}
+	
+Output: 17, 5, 2
+
---
+++ JavaScript DSA/JSDSA_DAY20/solution.js
@@ -0,0 +1,52 @@
+/*-----------------
+
+Naive Approach:
+The basic approach is to look for all the elements in a particular array.The outer loop runs from 0 to size-1 and one 
+by one picks up all the elements from left to right. The inner loop compares the picked element to all the elements to its 
+right side. If picked element is greater than all elements then it is a leader.But in this approach when you check through 
+every element and with every element the process becomes very slow. 
+
+Time Complexity: O(n^2)
+
+Efficient Approach:
+The solution to solve this in O(n) time period is to scan all the elements from right and keep track of max_from_right if its value
+changes i.e if a new maximum is observed then the previous value is printed.
+
+-----------------*/
+
+IMPLEMENTATION:
+
+let num = Number(readLine()); //taking the number if elements from the user as input
+var arr = [];
+
+//Taking the user input of different elements of array
+
+for(let i=0; i<num; i++){
+ let c = Number(readLine());
+ arr.push(c);
+}
+
+//Processing the input array from the user
+let z= "";
+for(let i = 0;i<N;i++){
+    z+=arr[i]+" ";
+}
+
+//Logging the input array from the user
+console.log(z);
+
+//Calling the maxSubArraySum method and logging the result in console
+leadersInAnArray(arr);
+
+function leadersInAnArray(arr){
+   var size = a.length; 
+   var max_from_right = arr[size-1];
+   System.out.println(max_from_right + " ");
+
+   for(let i = size-2; i >= 0; i--){
+	if(max_from_right <= arr[i]){
+	    max_from_right = arr[i];
+	    console.log(max_from_right + " ");
+	}        
+   }
+}
