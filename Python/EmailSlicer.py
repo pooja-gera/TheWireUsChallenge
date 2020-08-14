5da2e3d8b136e76d9edc4f45cfc11267ff2b099e
pooja-gera <55778515+pooja-gera@users.noreply.github.com>
2020-08-14 21:05:49 +0530

    Day 41 of EDA added

--- Python/EmailSlicer.py
+++
@@ -1,6 +0,0 @@
-#Source Code: Pooja Gera
-
-email = "abcdefgh@gmail.com"
-username,domainName = email.split("@")
-print("User Name: "+username)
-print("Domain Name: "+domainName)
--- Python/YoutubeVideoDownloader.py
+++
@@ -1,3 +0,0 @@
-from pytube import YouTube
-link = "https://www.youtube.com/watch?v=gCYcHz2k5x0"
-video = YouTube(link)
