f3307dda4fb6dd7dfaac594eb159dbfb3dbe46e7
Jappanjeet Singh <jappanjeet.99@gmail.com>
2020-08-22 00:30:26 +0530

    added challenges

---
+++ Web Scraping (Beautiful Soup, Scrapy, Selenium)/webScraping_Day67/Scrapy-/README.md
@@ -0,0 +1,26 @@
+# Challenge - Creating our First Spider
+Welcome Back!!👋
+
+Today we are going to be using a very quick and powerful library for scraping called **SCRAPY**.  
+###### Trust me it isn't Scary 🎃
+
+Scrapy is one of the most popular Python web scrapping libraries right now. It is an open-source framework. This means it is not even a library, it's rather a complete tool that you can use to scrape and crawl around the web systematically.
+
+Scrapy is designed to build web spiders that can crawl the web on their own.
+
+###### For installation guide, setting up a scrapy project and learning how to use scrapy refer to the resources.txt file.
+
+## Task 👨🏻‍💻👩🏻‍💻
+- Start of by making a scrapy project
+- in the spiders directory make your python file
+- make a class inheriting spider class
+- specify the urls - ["http://quotes.toscrape.com/page/1/",
+"http://quotes.toscrape.com/page/2/","http://quotes.toscrape.com/page/3/",] <br> in the start_requests method inside the spider class to scrape from.
+- parse the data using the parse method and scrape data like - ***text, authors and tags from each quote and yield a json object containing the data***.
+- finally run the web crawler with specifying the command - **scrapy crawl spider_name -o quotes.json** in your root directory of the project to save the file as a json.
+
+The file should have the contents starting something like - 
+<br>
+<img width="200px" src="./ss.png">
+
+
---
+++ Web Scraping (Beautiful Soup, Scrapy, Selenium)/webScraping_Day67/Scrapy-/first_project/first_project/spiders/quotes_spider.py
@@ -0,0 +1,53 @@
+# import the scrapy module
+import scrapy
+
+# Creating a spider
+# inherited subclass is Spider from scrapy module
+class QuotesSpider(scrapy.Spider):
+    # name of the spider | it must be unique within a project
+    name = "quotes_spider"
+
+    # define methods
+
+    # this method will allow us to make GET or POST requests
+    def start_requests(self):
+        # specifying urls to scrape data from
+        urls = [
+            "http://quotes.toscrape.com/page/1/",
+            "http://quotes.toscrape.com/page/2/",
+            "http://quotes.toscrape.com/page/3/",
+        ]
+
+        #   Generator
+        for url in urls:
+            # this statement is similar to making request to an API which we did in the challenges in past.
+            # callback method will define that when the response is received we have to parse the data
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    # a method that will be called to handle the response downloaded for each of the requests mad
+    def parse(self, response):
+        # in which page we are
+        page = response.url.split("/")[-2]
+        # define the filename to save the quotes
+        filename = "quotes-%s.html" % page
+
+        # find the quotes, authors and tags
+        quotes = response.css("div.quote")
+        # iterate over all the quotes
+        for q in quotes:
+            # finding the actual quote
+            text = q.css("span.text::text").get()
+            # finding authors
+            author = q.css("small.author::text").get()
+            # finding tags
+            tags = q.css("a.tag::text").getall()
+
+            # yield the results as json
+            yield {"text": text, "author": author, "tags": tags}
+
+        # this is for saving the html of the response(optional)
+        # with open(filename, "wb") as f:
+        #     f.write(response.body)
+        # # write a status message
+        # self.log("Saved file %s" % filename)
+
