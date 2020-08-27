5b30454a00010d7bf508f2ebd02e1770b71dbe2a
Prahitha <prahitha.movva03@gmail.com>
2020-08-27 17:28:18 +0530

    added challenge for day 60 (lcs)

--- JavaScript DSA/JSDSA_Day60/README.md
+++ JavaScript DSA/JSDSA_Day60/README.md
@@ -1,11 +1,9 @@
 ## Day 60
 ---
-### Tower of Hanoi
+### Longest Common Subsequence
 
-The Tower of Hanoi (also called the Tower of Brahma or Lucas' Tower) is a mathematical game. It consists of three rods and a number of disks of different sizes, which can slide onto any rod. The puzzle starts with the disks in a neat stack in ascending order of size on one rod, the smallest at the top, thus making a conical shape.
+The longest common subsequence problem is a classic computer science problem. It is the problem of finding the longest subsequence common to all sequences in a set of sequences (often just two sequences). 
 
-The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+**Note:** Unlike substrings, subsequences are not required to occupy consecutive positions within the original sequences.
 
-* Only one disk can be moved at a time.
-* Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack or on an empty rod.
-* No disk may be placed on top of a smaller disk.
+**Example:** Longest common subsequence for "ABCDEF" and "ACBEDF" is "ABDF" of length 4
--- JavaScript DSA/JSDSA_Day60/algo.js
+++ JavaScript DSA/JSDSA_Day60/algo.js
@@ -1,14 +1,56 @@
-// for n disks, total ((2^n) - 1) moves are required
-function towerOfHanoi(n, from, to, aux) {
-    if (n === 1) {
-        console.log("\n Move disk 1 from rod " + from + " to rod " + to);
-        return;
-    }
+// using recursion
+function usingRecursion(string1, s1, string2, s2) {
+    var result;
 
-    towerOfHanoi(n - 1, from, aux, to);
-    console.log("\n Move disk 1 from rod " + from + " to rod " + to);
-    towerOfHanoi(n - 1, aux, to, from);
+    // base case
+    if (!s1 || !s2)
+        result = 0;
+    
+    // if the character is same
+    else if (string1[s1 - 1] === string2[s2 - 1])
+        result = 1 + usingRecursion(string1, s1 - 1, string2, s2 - 1);
+    
+    else{
+        var checkString1 = usingRecursion(string1, s1 - 1, string2, s2);
+        var checkString2 = usingRecursion(string1, s1, string2, s2 - 1);
+
+        result = Math.max(checkString1, checkString2);
+    }
+    return result;
 }
 
-// considering only 3 disks on rod A
-towerOfHanoi(3, 'A', 'B', 'C');
+// using dynamic programming
+function usingDP(string1, s1, string2, s2) {
+    var array = new Array(s1 + 1).fill(new Array(s2 + 1));
+    var result;
+    // to store result for all combinations
+    if (array[s1][s2] !== undefined)
+        return array[s1][s2];
+
+    if (!s1 || !s2)
+        result = 0;
+    
+    else if (string1[s1 - 1] === string2[s2 - 1])
+        result = 1 + usingDP(string1, s1 - 1, string2, s2 - 1);
+
+    else {
+        var checkString1 = usingDP(string1, s1 - 1, string2, s2);
+        var checkString2 = usingDP(string1, s1, string2, s2 - 1);
+
+        result = Math.max(checkString1, checkString2);
+    }
+    array[s1][s2] = result;
+    return result;
+}
+
+var string1 = "ABCDEF";
+var string2 = "ACBEDF";
+length1 = string1.length;
+length2 = string2.length;
+
+
+recursion = usingRecursion(string1, length1, string2, length2);
+console.log("Using recursion: " + recursion);
+
+dp = usingDP(string1, length1, string2, length2);
+console.log("\nUsing DP: " + dp);
