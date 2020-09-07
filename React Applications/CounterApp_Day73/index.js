223be3bd512284234168f59d373d0a5a8505af7d
pooja-gera <55778515+pooja-gera@users.noreply.github.com>
2020-09-07 21:19:42 +0530

    Revert "Merge pull request #22 from yashodaAgarwal/yashoda"
    This reverts commit 244271e61e37b0968d79a38d6c2db76a84a7c07d, reversing
    changes made to 40dd7648e06a006cd2d36970984ee5dbfd269ed3.

--- React Applications/CounterApp_Day73/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
--- React Applications/settingUpLocalEnvironment_Day1/settingEnvir.js
+++
@@ -1,9 +0,0 @@
-//To create a new app
-npx create-react-app my-app
-
-//Once the installation is done, you can open your project folder:
-cd my-app
-
-//Runs the app in development mode.
-npm start
-
